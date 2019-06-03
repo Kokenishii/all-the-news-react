54ee1f498b392da8f6f73aaee0f7fa4248690c07
DannyBoyNYC <daniel.deverell@gmail.com>
2019-06-03 11:29:04 -0400

    done

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,40 +1,41 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+// https://reactgo.com/ajax-requests-fetch-data-using-react-hooks/
+// https://upmostly.com/tutorials/react-ajax-requests-fetch-data/
 const nytapi = 'uQG4jhIEHKHKm0qMKGcTHqUgAolr1GM0';
 const section = 'arts';
 
-const Stories = function() {
-  const getStories = function() {
-    fetch(
+const Stories = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  async function getStories() {
+    const response = await fetch(
       `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytapi}`,
-    )
-      .then(response => response.json())
-      .then(data => {
-        let stories = data.results.map(story => {
-          return (
-            <div className="entry" key={story.title}>
-              <img src={story.multimedia[0].url} alt="images" />
-              <div>
-                <h3>
-                  <a href={story.short_url}>
-                    {story.title}
-                  </a>
-                </h3>
-                <p>
-                  {story.abstract}
-                </p>
-              </div>
-            </div>
-          );
-        });
-      });
-  };
+    );
+    const json = await response.json();
+    setData(json);
+  }
 
   return (
-    <div className="site-wrap">
-      {getStories()}
+    <div className="entry" key={data.title}>
+      {data.title}
     </div>
   );
 };
 
 export default Stories;
+
+// {/* <img src={data.multimedia[0].url} alt="images" /> */}
+// <div>
+//   <h3>
+//     <a href={data.short_url}>
+//       {data.title}
+//     </a>
+//   </h3>
+//   <p>
+//     {data.abstract}
+//   </p>
+// </div>
