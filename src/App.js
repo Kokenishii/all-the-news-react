1eb935af68a5d4ca0ae76f3edbaf46442ad8cae6
DannyBoyNYC <daniel.deverell@gmail.com>
2019-06-04 07:51:39 -0400

    done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,42 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Stories from './components/Stories';
 
+const navItemsObject = [
+  {
+    label: 'arts',
+    link: '#arts',
+  },
+  {
+    label: 'books',
+    link: '#books',
+  },
+  {
+    label: 'fashion',
+    link: '#fashion',
+  },
+  {
+    label: 'food',
+    link: '#food',
+  },
+  {
+    label: 'movies',
+    link: '#movies',
+  },
+  {
+    label: 'travel',
+    link: '#travel',
+  },
+];
+
+function test() {
+  console.log('test');
+}
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Nav />
+      <Nav navList={navItemsObject} testFunc={test} />
       <Stories />
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,14 @@
 import React from 'react';
 
-const navItemsObject = [
-  {
-    label: 'arts',
-    link: '#arts',
-  },
-  {
-    label: 'books',
-    link: '#books',
-  },
-  {
-    label: 'fashion',
-    link: '#fashion',
-  },
-  {
-    label: 'food',
-    link: '#food',
-  },
-  {
-    label: 'movies',
-    link: '#movies',
-  },
-  {
-    label: 'travel',
-    link: '#travel',
-  },
-];
-
-const Nav = () => {
+const Nav = props => {
   return (
     <nav>
       <ul>
-        {navItemsObject.map(navItem => (
+        {props.navList.map(navItem => (
           <li key={navItem.link}>
-            <a href={navItem.link}>{navItem.label}</a>
+            <a onClick="this.props.test" href={navItem.link}>
+              {navItem.label}
+            </a>
           </li>
         ))}
       </ul>
